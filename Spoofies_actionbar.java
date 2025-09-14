28162281185619647fe0db335b836aec4b16e26a
TheSpoofies <sir.spoofsalots@gmail.com>
2025-09-14 22:26:08 +0100

    This is the only file

---
+++ Spoofies_actionbar.java
@@ -0,0 +1,10 @@
+package poseidon.event.spoofies_actionbar;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Spoofies_actionbar implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
